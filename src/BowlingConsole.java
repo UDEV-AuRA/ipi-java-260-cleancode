a25868250e299b5260874e7d636b6fdfd3c2c5b5
alexandrepetrillo <alexandre.petrillo@gmail.com>
2020-06-27 17:26:07 +0200

    init

--- src/BowlingConsole.java
+++ src/BowlingConsole.java
@@ -7,15 +7,20 @@ public class BowlingConsole {
 
         BowlingGame bowlingGame = new BowlingGame();
 
-        for (int i = 1; i <= 20; i++) {
-            System.out.println("Lancé " + i);
+
+        while (bowlingGame.isFinish()) {
+            System.out.println("Vous en êtes à l'étape " + bowlingGame.status());
+
             System.out.println("Combien de quille vous avez fait tombé ?");
             String nbQuillesAsString = scanner.nextLine();
             int nbQuilles = Integer.parseInt(nbQuillesAsString);
             bowlingGame.roll(nbQuilles);
+
             int score = bowlingGame.score();
             System.out.println("Votre score est de : " + score);
             System.out.println("-----------------");
         }
+        System.out.println("Le jeu est terminé. Votre score final est " + bowlingGame.score());
+
     }
 }
--- src/CoupInvalideException.java
+++
@@ -1,2 +0,0 @@
-public class CoupInvalideException extends RuntimeException {
-}
